5b242999aadf24fdf303ead9f6e0edb6d7984943
rajaganesan-s <114571974+rajaganesan-s@users.noreply.github.com>
2022-11-23 12:17:47 +0530

    Add files via upload

--- Project Development Phase/Sprint-4/main.py
+++ Project Development Phase/Sprint-4/main.py
@@ -1,73 +1,112 @@
-from flask import Flask, render_template,request, session
+import os.path
+
+from flask import Flask, render_template, request, session
 from flask_mysqldb import MySQL
+from flask import *
+import matplotlib.pyplot as plt
+import pandas as pd
 
-app=Flask(__name__)
+app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "db"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+mysql = MySQL(app)
 
-app.config["MYSQL_HOST"]="localhost"
-app.config["MYSQL_USER"]="root"
-app.config["MYSQL_PASSWORD"]=""
-app.config["MYSQL_DB"]="db"
-app.config["MYSQL_CURSORCLASS"]="DictCursor"
-mysql=MySQL(app)
+
 @app.route('/')
 @app.route('/raja')
 def index():
     return render_template("index.html")
-@app.route('/login',methods=['POST','GET'])
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.method=='POST':
-        username=request.form['uname']
-        password=request.form['upass']
-        con=mysql.connection.cursor()
-        sql="SELECT * FROM users where username='"+username+"' AND password='"+password+"'"
+    if request.method == 'POST':
+        username = request.form['uname']
+        password = request.form['upass']
+        con = mysql.connection.cursor()
+        sql = "SELECT * FROM users where username='" + username + "' AND password='" + password + "'"
         con.execute(sql)
-        result=con.fetchall()
+        result = con.fetchall()
         if result:
-            return "<h1>Login Successfully</h1>"
-            #return render_template("main.html")
+            return '<h1>Login Successfully <a href="/dash">Go to dashboard</a></h1>'
+            # return render_template("main.html")
         else:
             return "<h1>Incorrect Username and Password</h1>"
+
 
 @app.route('/users/<name>')
 def users(name):
     return "<h1>welcome {}</h1>".format(name)
 
+
+@app.route('/dash', methods=['GET', 'POST'])
+def dashboard():
+    if request.method == 'POST':
+        f = request.files['file']
+        if not os.path.isdir('static'):
+            os.mkdir('static')
+        filepath = os.path.join('static', f.filename)
+        f.save(filepath)
+        msg='The file name of uploaded file is: {}'.format(f.filename)
+        return '<h2>'+msg+'<br><a href="/dash/action">Get Graph</a></h2>'
+    else:
+        return render_template('dash.html',message="")
+
+@app.route('/dash/action', methods=['GET', 'POST'])
+def dashboardAction():
+    if request.method == 'POST':
+        variable = request.form['variable']
+        data = pd.read_csv('static/test.csv')
+        plt.plot(data[variable])
+        imagepath = os.path.join('static', 'image3' + '.png')
+        plt.savefig(imagepath)
+        plt.close()
+        return render_template('image.html')
+    return render_template('dashaction.html')
+
+
 @app.route('/db')
 def db():
-    con=mysql.connection.cursor()
-    sql="SELECT * FROM users where username='raja' AND password='raja'"
+    con = mysql.connection.cursor()
+    sql = "SELECT * FROM users where username='raja' AND password='raja'"
     con.execute(sql)
-    result=con.fetchall()
+    result = con.fetchall()
     if result:
-        t="yes"
-        return render_template("db.html",datas=result,t=t)
+        t = "yes"
+        return render_template("db.html", datas=result, t=t)
     else:
-        t="no"
-        return render_template("db.html",t=t)
+        t = "no"
+        return render_template("db.html", t=t)
+
 
 @app.route('/register')
 def register():
     return render_template("register.html");
 
-@app.route('/register-action',methods=['POST','GET'])
+
+@app.route('/register-action', methods=['POST', 'GET'])
 def registerAction():
-    if request.method=='POST':
-        email=request.form['email']
-        psw=request.form['psw']
-        pswRepeat=request.form['psw-repeat']
-        if psw==pswRepeat:
-            con=mysql.connection.cursor()
-            sql1="select * from users where username='"+email+"'"
+    if request.method == 'POST':
+        email = request.form['email']
+        psw = request.form['psw']
+        pswRepeat = request.form['psw-repeat']
+        if psw == pswRepeat:
+            con = mysql.connection.cursor()
+            sql1 = "select * from users where username='" + email + "'"
             con.execute(sql1)
-            result=con.fetchall()
+            result = con.fetchall()
             if result:
                 return "<h2>This Email is already registered try another Email address</h1>"
             else:
-                sql="INSERT INTO users (username,password) VALUES('"+email+"','"+psw+"')"
-                status=con.execute(sql)
+                sql = "INSERT INTO users (username,password) VALUES('" + email + "','" + psw + "')"
+                status = con.execute(sql)
                 mysql.connection.commit()
                 if status:
-                    return "<h3>Successfully Registered</h3>"
+                    return '<h3>Successfully Registered. Go to <a href="/">Login Page</a></h3>'
                 else:
                     return "<h3>Error</h3>"
         else:
@@ -75,14 +114,15 @@ def registerAction():
     else:
         return "<h1>dkjfd</h1>"
 
-@app.route('/logout')
+
+""""@app.route('/logout')
 def logout():
 	session.pop('loggedin', None)
 	session.pop('id', None)
 	session.pop('username', None)
 	return render_template("login.html")
 
-"""@app.route('/add_stock', methods =['GET', 'POST'])
+@app.route('/add_stock', methods =['GET', 'POST'])
 def add_stock():
 	msg = ''
 	stocks = dbname["Stocks"]
@@ -137,5 +177,5 @@ def add_sale(&):
 		stock = stocks.find_one({"name" : request.args.get("name")})
 	return render_template('add_sale.html', stock = stock)	
 """
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
